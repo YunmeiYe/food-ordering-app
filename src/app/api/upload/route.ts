c553d3adca31a2ba9dff6b75f406ea19d1131ed7
YunmeiYe <yunmeiye.au@gmail.com>
2024-01-24 09:36:38 +1030

    fix: Cloudinary upload_stream returns 405 on production

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,31 +1,47 @@
 import cloudinary from 'cloudinary';
 import { NextRequest, NextResponse } from 'next/server';
 
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRECT,
+  secure: true
+});
+
 export async function POST(req: NextRequest) {
-  cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRECT,
-    secure: true
-  });
 
   const data = await req.formData();
 
   if (data.get('file')) {
     const file = data.get('file') as File;
-    const arrayBuffer = await file.arrayBuffer();
-    const buffer = new Uint8Array(arrayBuffer);
+    const fileBuffer = await file.arrayBuffer();
 
-    const uploadResult: any = await new Promise((resolve, reject) => {
-      cloudinary.v2.uploader.upload_stream({}, function (error, result) {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve(result)
-      }).end(buffer);
-    })
+    var mime = file.type;
+    var encoding = 'base64';
+    var base64Data = Buffer.from(fileBuffer).toString('base64');
+    var fileUri = 'data:' + mime + ';' + encoding + ',' + base64Data;
 
-    return NextResponse.json(uploadResult.secure_url)
+    try {
+      const uploadToCloudinary = () => {
+        return new Promise((resolve, reject) => {
+          cloudinary.v2.uploader.upload(fileUri, {
+            invalidate: true
+          })
+            .then((result) => {
+              console.log(result);
+              resolve(result);
+            })
+            .catch((error) => {
+              console.log(error);
+              reject(error);
+            });
+        });
+      };
+      const uploadResult: any = await uploadToCloudinary();
+      return NextResponse.json(uploadResult.secure_url)
+    } catch (error) {
+      console.log("server err", error);
+      return NextResponse.json({ err: "Internal Server Error" }, { status: 500 });
+    }
   }
 }
